26b870f88dd146056ae8f9ac863ec687cf66cf18
marthin salakory <marthinsalakory11@gmail.com>
2021-03-27 14:05:02 +0900

    save saja

--- app/Models/usersModel.php
+++ app/Models/usersModel.php
@@ -9,17 +9,12 @@ class usersModel extends Model
     protected $table = 'users';
     protected $useTimestamps = true;
     // protected $primaryKey = 'user_id';
-    protected $allowedFields = ['email', 'username', 'nama_lengkap', 'level', 'no_telp', 'user_image', 'document', 'bio', 'password_hash', 'active'];
+    protected $allowedFields = ['email', 'username', 'nama_lengkap', 'level', 'no_telp', 'user_image', 'document', 'password_hash', 'active'];
 
     public function getuser($id = false)
     {
 
         if ($id == false) {
-            //     $query =  $this->select('message.id as messageid, pengirim, penerima, message.created_at as date, nama_lengkap')
-            //     ->join('users u', 'u.id = message.penerima')
-            //     ->get();
-
-            // return $query->getResultArray();
             return $this->findAll();
         }
 
